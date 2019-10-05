0ee44ae51a0d1edbc6dad7b281fcafa945dbe6d4
rajendra77 <rs26105@gmail.com>
2019-10-05 22:18:49 +0530

    intial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+class App extends React.Component{
+      render(){
+        return(
+         <h2>Hello world</h2>
+        )
+      }  
+}
+export default App;
